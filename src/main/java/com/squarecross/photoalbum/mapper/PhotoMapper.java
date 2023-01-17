81aa153fe9855c700a6789cd07addcbbbadbf22a
Kyle <peyopeyo92@gmail.com>
2023-01-18 01:14:01 +0900

    Get Photo Details API added

--- src/main/java/com/squarecross/photoalbum/mapper/PhotoMapper.java
+++ src/main/java/com/squarecross/photoalbum/mapper/PhotoMapper.java
@@ -1,4 +1,19 @@
 package com.squarecross.photoalbum.mapper;
 
+import com.squarecross.photoalbum.Constants;
+import com.squarecross.photoalbum.domain.Photo;
+import com.squarecross.photoalbum.dto.PhotoDto;
+
 public class PhotoMapper {
+    public static PhotoDto convertToDto(Photo photo){
+        PhotoDto photoDto = new PhotoDto();
+        photoDto.setPhotoId(photo.getPhotoId());
+        photoDto.setOriginalUrl(Constants.PATH_PREFIX + photo.getOriginalUrl());
+        photoDto.setFileSize(photo.getFileSize());
+        photoDto.setThumbUrl(Constants.PATH_PREFIX + photo.getThumbUrl());
+        photoDto.setAlbumId(photo.getAlbum().getAlbumId());
+        photoDto.setUploadedAt(photo.getUploadedAt());
+        photoDto.setFileName(photo.getFileName());
+        return photoDto;
+    }
 }
--- src/main/java/com/squarecross/photoalbum/service/PhotoService.java
+++ src/main/java/com/squarecross/photoalbum/service/PhotoService.java
@@ -1,11 +1,25 @@
 package com.squarecross.photoalbum.service;
 
+import com.squarecross.photoalbum.domain.Photo;
+import com.squarecross.photoalbum.dto.PhotoDto;
+import com.squarecross.photoalbum.mapper.PhotoMapper;
 import com.squarecross.photoalbum.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
 
 @Service
 public class PhotoService {
     @Autowired
     private PhotoRepository photoRepository;
+
+    public PhotoDto getPhotoById(Long photoId) {
+        Optional<Photo> res = photoRepository.findById(photoId);
+        if(res.isEmpty()){
+            throw new EntityNotFoundException(String.format("파일 아이디 %d 를 찾을 수 없습니다", photoId));
+        }
+        return PhotoMapper.convertToDto(res.get());
+    }
 }
