a3b2bd14784e6b605aa36d3ddeedda0e6494cb20
Kyle <peyopeyo92@gmail.com>
2023-01-30 21:26:58 +0900

    프로젝트 Final

--- src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
+++ src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
@@ -2,13 +2,20 @@ package com.squarecross.photoalbum.controller;
 
 import com.squarecross.photoalbum.dto.PhotoDto;
 import com.squarecross.photoalbum.service.PhotoService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 @RequestMapping("/albums/{albumId}/photos")
@@ -20,5 +27,45 @@ public class PhotoController {
     public ResponseEntity<PhotoDto> getPhotoInfo(@PathVariable("photoId") final Long photoId){
         PhotoDto photoDto = photoService.getPhoto(photoId);
         return new ResponseEntity<>(photoDto, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="", method = RequestMethod.POST)
+    public ResponseEntity<List<PhotoDto>> uploadPhotos(@PathVariable("albumId") final Long albumId,
+                                                       @RequestParam("photos") MultipartFile[] files) {
+        List<PhotoDto> photos = new ArrayList<>();
+        for (MultipartFile file : files) {
+            PhotoDto photoDto = photoService.savePhoto(file, albumId);
+            photos.add(photoDto);
+        }
+        return new ResponseEntity<>(photos, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/download", method = RequestMethod.GET)
+    public void downloadPhotos(@RequestParam("photoIds") Long[] photoIds, HttpServletResponse response){
+        try {
+            if (photoIds.length == 1){
+                File file = photoService.getImageFile(photoIds[0]);
+                OutputStream outputStream = response.getOutputStream();
+                IOUtils.copy(new FileInputStream(file), outputStream);
+                response.flushBuffer();
+            } else {
+                ZipOutputStream zipOut = new ZipOutputStream(response.getOutputStream());
+                for (Long photoId : photoIds) {
+                    File file = photoService.getImageFile(photoId);
+                    ZipEntry zipEntry = new ZipEntry(file.getName());
+                    zipEntry.setSize(file.length());
+                    zipOut.putNextEntry(zipEntry);
+                    IOUtils.copy(new FileInputStream(file), zipOut);
+                    zipOut.closeEntry();
+                }
+                zipOut.finish();
+                zipOut.close();
+                response.flushBuffer();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Error");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/squarecross/photoalbum/service/PhotoService.java
+++ src/main/java/com/squarecross/photoalbum/service/PhotoService.java
@@ -1,13 +1,25 @@
 package com.squarecross.photoalbum.service;
 
+import com.squarecross.photoalbum.Constants;
+import com.squarecross.photoalbum.domain.Album;
 import com.squarecross.photoalbum.domain.Photo;
 import com.squarecross.photoalbum.dto.PhotoDto;
 import com.squarecross.photoalbum.mapper.PhotoMapper;
+import com.squarecross.photoalbum.repository.AlbumRepository;
 import com.squarecross.photoalbum.repository.PhotoRepository;
+import org.imgscalr.Scalr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import javax.persistence.EntityNotFoundException;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 @Service
@@ -15,11 +27,84 @@ public class PhotoService {
     @Autowired
     private PhotoRepository photoRepository;
 
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    private final String original_path = Constants.PATH_PREFIX + "/photos/original";
+    private final String thumb_path = Constants.PATH_PREFIX + "/photos/thumb";
+
     public PhotoDto getPhoto(Long photoId) {
         Optional<Photo> res = photoRepository.findById(photoId);
         if(res.isEmpty()){
             throw new EntityNotFoundException(String.format("파일 아이디 %d 를 찾을 수 없습니다", photoId));
         }
         return PhotoMapper.convertToDto(res.get());
+    }
+
+    public PhotoDto savePhoto(MultipartFile file, Long albumId){
+        Optional<Album> res = albumRepository.findById(albumId);
+        if(res.isEmpty()){
+            throw new EntityNotFoundException("앨범이 존재하지 않습니다");
+        }
+        //같은 파일명이 존재하면 파일명에 숫자를 붙인다
+        String fileName = file.getOriginalFilename();
+        int fileSize = (int)file.getSize();
+        fileName = getNextFileName(fileName, albumId);
+
+
+        //파일 저장에 성공하면 DB에 저장한다
+        saveFile(file, albumId, fileName);
+
+        Photo photo = new Photo();
+        photo.setOriginalUrl("/photos/original/" + albumId + "/" + fileName);
+        photo.setThumbUrl("/photos/thumb/" + albumId + "/" + fileName);
+        photo.setFileName(fileName);
+        photo.setAlbum(res.get());
+        photo.setFileSize(fileSize);
+        Photo createdPhoto = photoRepository.save(photo);
+        return PhotoMapper.convertToDto(createdPhoto);
+    }
+
+
+
+    private String getNextFileName(String fileName, Long albumId){
+        String fileNameNoExt = StringUtils.stripFilenameExtension(fileName);
+        String ext = StringUtils.getFilenameExtension(fileName);
+
+        Optional<Photo> res = photoRepository.findByFileNameAndAlbum_AlbumId(fileName, albumId);
+        int count = 2;
+
+        while(res.isPresent()){
+            fileName = String.format("%s (%d).%s", fileNameNoExt, count, ext);
+            res = photoRepository.findByFileNameAndAlbum_AlbumId(fileName, albumId);
+            count++;
+        }
+
+        return fileName;
+    }
+
+    private void saveFile(MultipartFile file, Long AlbumId, String fileName) {
+        try {
+            String filePath = AlbumId + "/" + fileName;
+            Files.copy(file.getInputStream(), Paths.get(original_path + "/" + filePath));
+
+            BufferedImage thumbImg = Scalr.resize(ImageIO.read(file.getInputStream()), Constants.THUMB_SIZE, Constants.THUMB_SIZE);
+            File thumbFile = new File(thumb_path + "/" + filePath);
+            String ext = StringUtils.getFilenameExtension(fileName);
+            if (ext == null) {
+                throw new IllegalArgumentException("No Extention");
+            }
+            ImageIO.write(thumbImg, ext, thumbFile);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+        }
+    }
+
+    public File getImageFile(Long photoId) {
+        Optional<Photo> res = photoRepository.findById(photoId);
+        if(res.isEmpty()){
+            throw new EntityNotFoundException(String.format("사진을 ID %d를 찾을 수 없습니다", photoId));
+        }
+        return new File(Constants.PATH_PREFIX + res.get().getOriginalUrl());
     }
 }
